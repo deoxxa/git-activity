427578677922c00185a8d97f5cd8dc59e1fb5efd
deoxxa <deoxxa@fknsrs.biz>
2015-06-27 16:43:23 +1000

    document what the command does and how

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
+/*
+
+git-activity infers relationships between files in a git repository.
+
+It outputs dot data that can be rendered using graphviz like so:
+
+  git-activity | graphviz -Tpdf -o graph.pdf
+
+It executes git command-line programs, so they need to be installed for this
+tool to work. You also need to run this tool in the repository you'd like to
+analyse.
+
+The way git-activity determines relationships is by looking at the commit
+history of a repository and recording, against each file, a list of other files
+that have been edited in the same commit, along with the number of times this
+has happened.
+
+*/
 package main
 
 import (
